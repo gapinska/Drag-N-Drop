6486ce6806242d447fad55634e59b10342e7dab9
gapinska <gapinska.mon@gmail.com>
2021-03-12 07:43:05 +0100

    update

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -1,14 +1,45 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
 const DragNDrop = ({ data }) => {
   const [list, setList] = useState(data)
+  const [dragging, setDragging] = useState(false)
+
+  const dragItem = useRef()
+
+  const handleDragStart = (e, params) => {
+    console.log("drag starting", params)
+    dragItem.current = params
+    setDragging(true)
+  }
+
+  const getStyles = (params) => {
+    const currentItem = dragItem.current
+    if (
+      currentItem.dndGroupId === params.dndGroupId &&
+      currentItem.itemId === params.itemId
+    ) {
+      return "current dnd-item"
+    } else {
+      return "dnd-item"
+    }
+  }
+
   return (
     <div className="drag-n-drop">
-      {list.map((grp, grpI) => (
-        <div key={grp.title} className="dnd-group">
-          <div className="group-title">{grp.title}</div>
-          {grp.items.map((item, itemI) => (
-            <div draggable key={item} className="dnd-item">
+      {list.map((dndGroup, dndGroupId) => (
+        <div key={dndGroup.title} className="dnd-group">
+          <div className="group-title">{dndGroup.title}</div>
+          {dndGroup.items.map((item, itemId) => (
+            <div
+              draggable
+              onDragStart={(e) => {
+                handleDragStart(e, { dndGroupId, itemId })
+              }}
+              key={item}
+              className={
+                dragging ? getStyles({ dndGroupId, itemId }) : "dnd-item"
+              }
+            >
               {item}
             </div>
           ))}
